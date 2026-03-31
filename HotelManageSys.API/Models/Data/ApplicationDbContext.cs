fefe9269b3e1d02a014fb31e93e493c68bd4bba6
olafenko <olopikolo666@gmail.com>
2026-03-31 15:32:42 +0200

    feature/ init dbcontext class

---
+++ HotelManageSys.API/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelManageSys.API.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Amenity> Amenities { get; set; }
+        public DbSet<HotelService> HotelServices { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+
+
+    }
+}
