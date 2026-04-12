53bf5b7316fbe3ee157571e862eacbb754ec8ef4
olafenko <olopikolo666@gmail.com>
2026-04-12 18:28:30 +0200

    feature/ add base of navigation types

---
+++ HotelManageSysMobile/src/navigation/types.ts
@@ -0,0 +1,19 @@
+﻿import type { Room } from '../types/models';
+
+
+export type RootStackParamList = {
+    
+    Home: undefined;
+    
+    Rooms: undefined;
+    CreateRoom: undefined;
+    EditRoom: { room: Room};
+    
+    RoomTypes: undefined;
+    Reservations: undefined;
+    Workers: undefined;
+    Guests: undefined;
+    Amenities: undefined;
+    AdditionalOffers: undefined;
+    
+}
