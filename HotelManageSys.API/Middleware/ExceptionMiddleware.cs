8eee5abf3937478db8eb84e3f1e29b46662274ae
olafenko <olopikolo666@gmail.com>
2026-07-04 19:54:39 +0200

    feature/ add switch for exception types

--- HotelManageSys.API/Middleware/ExceptionMiddleware.cs
+++ HotelManageSys.API/Middleware/ExceptionMiddleware.cs
@@ -1,4 +1,6 @@
-﻿using HotelManageSys.API.Exceptions;
+﻿using System.Net;
+using System.Text.Json;
+using HotelManageSys.API.Exceptions;
 
 namespace HotelManageSys.API.Middleware;
 
@@ -37,8 +39,77 @@ public class ExceptionMiddleware
 
         var response = new ErrorResponse();
 
-        //todo dodac mapsera dla response dla odpowiedniego errora
+        //todo dodac mapsera dla ErrorResponse dla odpowiedniego errora
         
+        switch (exception)
+            {
+                // Błędy walidacji - 400 Bad Request
+                case ValidationException validationEx:
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    response.Type = "ValidationError";
+                    response.Title = "Błędy walidacji";
+                    response.Status = 400;
+                    response.Errors = validationEx.Errors;
+                    break;
 
+                // Nie znaleziono - 404 Not Found
+                case NotFoundException notFoundEx:
+                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.Type = "NotFound";
+                    response.Title = notFoundEx.Message;
+                    response.Status = 404;
+                    break;
+
+                // Nieprawidłowy argument - 400 Bad Request
+                case ArgumentException argEx:
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    response.Type = "BadRequest";
+                    response.Title = argEx.Message;
+                    response.Status = 400;
+                    break;
+
+                // Brak autoryzacji - 401 Unauthorized
+                case UnauthorizedAccessException:
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    response.Type = "Unauthorized";
+                    response.Title = "Brak autoryzacji";
+                    response.Status = 401;
+                    break;
+
+                // Operacja niedozwolona - 403 Forbidden
+                case InvalidOperationException invalidOpEx:
+                    context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                    response.Type = "Forbidden";
+                    response.Title = invalidOpEx.Message;
+                    response.Status = 403;
+                    break;
+
+                // Wszystko inne - 500 Internal Server Error
+                default:
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    response.Type = "InternalError";
+                    response.Title = _environment.IsDevelopment()
+                        ? exception.Message
+                        : "Wystąpił błąd serwera";
+                    response.Status = 500;
+
+                    // W development dodaj stack trace
+                    if (_environment.IsDevelopment())
+                    {
+                        response.Detail = exception.StackTrace;
+                    }
+                    break;
+            }
+
+            // Dodaj TraceId dla śledzenia
+            response.TraceId = context.TraceIdentifier;
+
+            var json = JsonSerializer.Serialize(response, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = _environment.IsDevelopment()
+            });
+
+            await context.Response.WriteAsync(json);
     }
 }
