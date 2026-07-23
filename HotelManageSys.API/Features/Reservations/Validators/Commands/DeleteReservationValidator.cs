e3efda4ba4ef1b9b138c1eaa73ee90700cf34cd5
olafenko <olopikolo666@gmail.com>
2026-07-23 18:17:31 +0200

    feature/ add validation for delete reservation and get reservation by id

---
+++ HotelManageSys.API/Features/Reservations/Validators/Commands/DeleteReservationValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using HotelManageSys.API.Features.Reservations.Messages.Commands;
+
+namespace HotelManageSys.API.Features.Reservations.Validators.Commands;
+
+public class DeleteReservationValidator : AbstractValidator<DeleteReservationCommand>
+{
+    public DeleteReservationValidator()
+    {
+
+        RuleFor(x => x.Id)
+            .GreaterThan(0)
+            .WithMessage("Nieprawidłowe ID rezerwacji");
+
+    }
+}
